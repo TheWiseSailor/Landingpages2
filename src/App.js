081ceb7c5e46a17b67067a45d07a15758af7cb6a
TheWiseSailor <rudasilljay@gmail.com>
2024-05-06 18:06:01 -0400

    preset layout configs starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import HomeRender from './Components/HomeRender';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes> 
+          <Route path="/" element={<HomeRender />} /> 
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+//makemore improvements to future landing pages
