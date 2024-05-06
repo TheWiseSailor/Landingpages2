93a7256016c84cc0e23d22f61c9d5801409ea1e0
TheWiseSailor <rudasilljay@gmail.com>
2024-05-06 19:00:46 -0400

    logo made header done for the most part

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,35 +1,62 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import logo from './HeaderImages/logo.png';
-import './Header.css';
+import './Header.css'; 
 
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isContactClicked, setIsContactClicked] = useState(false);
+  const [isResumeClicked, setIsResumeClicked] = useState(false); 
+  const [isNewsClicked, setIsNewsClicked] = useState(false); 
+  const [isPortfolioClicked, setIsPortfolioClicked] = useState(false); 
+  const [isPropertiesClicked, setIsPropertiesClicked] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const { pathname } = location;
+    setIsContactClicked(pathname.toLowerCase() === '/properties');
+    setIsNewsClicked(pathname.toLowerCase() === '/news');
+    setIsPortfolioClicked(pathname.toLowerCase() === '/contact');
+    setIsPropertiesClicked(pathname.toLowerCase() === '/properties');
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [location]);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
-    <header>
-      <div className={`HeaderPrimary ${isMobileMenuOpen ? 'esports' : ''}`}>
-        <div className="container header">
-          <div className="logo-container">
-            <Link to="/">
-              <img src={logo} alt="Logo" className="logo" />
-            </Link>
-          </div>
-          <nav className={`navbar ${isMobileMenuOpen ? 'open' : ''}`}>
-            <ul>
-              <li><Link to="/portfolio">Portfolio</Link></li>
-              <li><Link to="/contacts">Contacts</Link></li>
-            </ul>
-          </nav>
-          <div className={`mobile-menu-icon ${isMobileMenuOpen ? 'active' : ''}`} onClick={toggleMobileMenu}>
-            <FontAwesomeIcon icon={faBars} className='mobile-menu-icon MMI-icon' />
-          </div>
+    <header className={`HeaderPrimary ${scrolling ? 'hidden' : ''} ${isContactClicked ? 'contact' : ''} ${isResumeClicked ? 'resume' : ''} ${isNewsClicked ? 'news' : ''} ${isPortfolioClicked ? 'portfolio' : ''}${isPropertiesClicked ? 'properties' : ''}`}>
+      <div className="container header">
+        <div className="logo-container">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </Link>
+        </div>
+        <nav className={`navbar ${isMobileMenuOpen ? 'open' : ''}`}>
+          <ul>
+            <li className='nav-li'><Link to="/properties" className={isResumeClicked ? 'black-and-white' : ''}>Ice Cream</Link></li>
+            <li className='nav-li'><Link to="/news" className={isPortfolioClicked ? 'black-and-white' : ''}>Bakery</Link></li>
+            <li className='nav-li'><Link to="/contact" className={isContactClicked ? 'black-and-white' : ''}>Gift Shop</Link></li>
+          </ul>
+        </nav>
+        <div className={`mobile-menu-icon ${isMobileMenuOpen ? 'active' : ''}`} onClick={toggleMobileMenu}>
+          <FontAwesomeIcon icon={faBars} className='mobile-menu-icon MMI-icon' />
         </div>
       </div>
     </header>
