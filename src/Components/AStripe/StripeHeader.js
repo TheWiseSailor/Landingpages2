e615cca6b99e48e22f3acd761e548d62b67a57e9
TheWiseSailor <rudasilljay@gmail.com>
2024-07-19 17:13:13 -0400

    css tweakin

--- src/Components/AStripe/StripeHeader.js
+++ src/Components/AStripe/StripeHeader.js
@@ -5,9 +5,9 @@ import './Stripe.css'; // Ensure you import the CSS file
 const StripeHeader = () => {
   return (
     <div className="stripe-header">
-      <p>116 E. Warren Street, Shelby, NC | (704) 481-7285</p>
-      <p></p>
-      <p>Monday-Saturday 11 a.m. - 10 p.m. | Closed Sunday</p>
+      <p>116 E. Warren Street, Shelby, NC | (704) 481-7285 | Monday-Saturday 11 a.m. - 10 p.m. | Closed Sunday</p>
+      
+
 
       <a href="/order-online">ORDER ONLINE</a>
     </div>
